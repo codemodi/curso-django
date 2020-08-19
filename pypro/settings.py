d4ff8b9d3c3a892187749799f4e6a583ec2bfb18
pcampos119104 <pcampos1191041@gmail.com>
2020-08-19 08:02:10 -0300

    Secret key isolated with Decouple
    close #18

--- pypro/settings.py
+++ pypro/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aeh_ee&x40a^m@wleg_$+wv#(z4@**7(a#8fja*b^%9s-eq139'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
