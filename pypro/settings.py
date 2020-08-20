a59bf4c7bb1e6e6930182cbcab760628796699b2
pcampos119104 <pcampos1191041@gmail.com>
2020-08-20 08:22:31 -0300

    Configurated static and media directories
    close #29

--- pypro/settings.py
+++ pypro/settings.py
@@ -116,4 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# dev config
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
