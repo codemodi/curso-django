61fb95c5104775b31155ba5d0c8c5f38e54b93e3
pcampos119104 <pcampos1191041@gmail.com>
2020-08-21 06:29:55 -0300

    added env COLLECTFAST_STRATEGY

--- pypro/settings.py
+++ pypro/settings.py
@@ -140,6 +140,7 @@ if AWS_ACCESS_KEY_ID != '':
     AWS_DEFAULT_ACL = 'private'
 
     COLLECTFAST_ENABLE = True
+    COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 
     # Static Assets
     STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
