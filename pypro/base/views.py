6de5468a9f5d296bda4bf43581043f3c198ed876
pcampos119104 <pcampos1191041@gmail.com>
2020-08-24 08:12:06 -0300

    Set up the Sentry

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('<html><body>Hello Django</body></html>', content_type='text/html')
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
