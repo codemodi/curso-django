b5725ea7bfef644702e65ed82a8cb782b4530093
pcampos119104 <pcampos1191041@gmail.com>
2020-08-24 17:15:48 -0300

    Landing page ok

--- pypro/urls.py
+++ pypro/urls.py
@@ -17,7 +17,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-from pypro.base.views import home, trigger_error
+from pypro.base.views import trigger_error
 
 urlpatterns = [
     path('admin/', admin.site.urls),
