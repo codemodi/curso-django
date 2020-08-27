e5ce3a9d7f29dd674c9927e66bf1047fd245b802
pcampos119104 <pcampos1191041@gmail.com>
2020-08-27 05:33:03 -0300

    HTTP and Templates lessons
    close #56

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -15,8 +15,12 @@ def test_status_code(resp):
 
 
 def test_title(resp):
-    assert_contains(resp, '<title>Python Pro</title>')
+    assert_contains(resp, '<title>Python Pro - Home</title>')
 
 
 def test_home_link(resp):
     assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
+
+
+def test_phone_link(resp):
+    assert_contains(resp, '+55 11 xxxxx-xxxx')
