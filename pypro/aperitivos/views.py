e1122efa12868c9e80921830b084e99a65b47a1b
pcampos119104 <pcampos1191041@gmail.com>
2020-09-02 08:29:25 -0300

    Testes com Banco de Dados

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,29 +1,13 @@
-from django.shortcuts import render
-from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
 
-
-class Video:
-    def __init__(self, slug, titulo, vimeo_id):
-        self.titulo = titulo
-        self.slug = slug
-        self.vimeo_id = vimeo_id
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
-
-
-videos = [
-    Video(slug='motivacao', titulo='We are All Going To Die', vimeo_id=292711054),
-    Video(slug='super-generic', titulo='Super Generic', vimeo_id=415570097)
-]
-
-videos_dct = {v.slug: v for v in videos}
+from pypro.aperitivos.models import Video
 
 
 def video(request, slug):
-    video = videos_dct[slug]
+    video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
 
 
 def indice(request):
-    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+    v = Video.objects.order_by('creation').all()
+    return render(request, 'aperitivos/indice.html', context={'videos': v})
